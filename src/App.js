dfde0c2cf9acee50cc41302d4a0a4789c975b49f
LakshithaV <2018csc017@univ.jfn.ac.lk>
2021-10-03 07:38:27 +0530

    BookDetails created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BookDetails from "./components/BookDetails";
+import Create from "./components/create";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
@@ -11,6 +13,12 @@ function App() {
           <Switch>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+            <Route exact path="/books/:id">
+              <BookDetails />
             </Route>
           </Switch>
         </div>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 const BookList = ({ books }) => {
   return (
     <div className="book-list">
       {books.map((book) => (
         <div className="book-preview" key={book.id}>
-          <h2>{book.title}</h2>
-          <p>Written by {book.author}</p>
+          <Link to={`/books/${book.id}`}>
+            <h2>{book.title}</h2>
+            <p>Written by {book.author}</p>
+          </Link>
         </div>
       ))}
     </div>
